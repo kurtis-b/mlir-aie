c9be5d73ba1374989f3f8eb3d647b257c98c5cc9
kurtis-b <bcurtjohn@gmail.com>
2025-04-19 04:36:03 -0700

    Add and norm operator working in full design

--- programming_examples/ml/transformer/layer/test.cpp
+++ programming_examples/ml/transformer/layer/test.cpp
@@ -49,20 +49,20 @@ using ACC_DATATYPE = DTYPE_ACC;
 constexpr long long verify_stochastic_threshold = 1024 * 1024 * 1024;
 constexpr int verify_stochastic_n_samples = 1000;
 
-// Verification tolerance
-// See "Note on Numerical Tolerances" in README.md
-float abs_tol = matmul_common::get_abs_tol<C_DATATYPE>();
-float rel_tol = matmul_common::get_rel_tol<C_DATATYPE>();
-
 namespace po = boost::program_options;
 
 int ffn1(const po::variables_map &vm, xrt::device &device, xrt::xclbin &xclbin,
-         xrt::hw_context &context, std::vector<C_DATATYPE> &outVec,
-         const int verbosity, const int do_verify, const int n_iterations,
+         xrt::hw_context &context, std::vector<A_DATATYPE> &inVec,
+         std::vector<C_DATATYPE> &outVec, const int verbosity,
+         const int do_verify, const int n_iterations,
          const int n_warmup_iterations, const int trace_size,
          const int b_col_maj) {
   // Fix the seed to ensure reproducibility in CI.
   srand(1726250518); // srand(time(NULL));
+  // Verification tolerance
+  // See "Note on Numerical Tolerances" in README.md
+  float abs_tol = matmul_common::get_abs_tol<C_DATATYPE>();
+  float rel_tol = matmul_common::get_rel_tol<C_DATATYPE>();
 
   int M = vm["M1"].as<int>();
   int K = vm["K1"].as<int>();
@@ -211,12 +211,15 @@ int ffn1(const po::variables_map &vm, xrt::device &device, xrt::xclbin &xclbin,
       if (do_verify_stochastic) {
         errors = matmul_common::verify_stochastic<A_DATATYPE, C_DATATYPE,
                                                   ACC_DATATYPE>(
-            M, N, K, AVec, BVec, CVec, true, verify_stochastic_n_samples,
-            verbosity, abs_tol, rel_tol, b_col_maj);
+            M, N, K, AVec, BVec, CVec,
+            matmul_common::kernel_op_t::MATMUL_FUSED_OP,
+            verify_stochastic_n_samples, verbosity, abs_tol, rel_tol,
+            b_col_maj);
       } else {
         errors = matmul_common::verify<A_DATATYPE, C_DATATYPE, ACC_DATATYPE>(
-            M, N, K, AVec, BVec, CVec, true, verbosity, abs_tol, rel_tol,
-            b_col_maj);
+            M, N, K, AVec, BVec, CVec,
+            matmul_common::kernel_op_t::MATMUL_FUSED_OP, verbosity, abs_tol,
+            rel_tol, b_col_maj);
       }
       auto vstop = std::chrono::system_clock::now();
       float vtime =
@@ -271,6 +274,7 @@ int ffn1(const po::variables_map &vm, xrt::device &device, xrt::xclbin &xclbin,
   //             << "Avg CPU matmul time: " << cpu_time_total / (num_iter / 5)
   //             << "us." << std::endl;
 
+  inVec = AVec;
   outVec = CVec;
   if (!errors) {
     std::cout << "\nPASS!\n\n";
@@ -289,13 +293,17 @@ int ffn1(const po::variables_map &vm, xrt::device &device, xrt::xclbin &xclbin,
 }
 
 int ffn2(const po::variables_map &vm, xrt::device &device, xrt::xclbin &xclbin,
-         xrt::hw_context &context, std::vector<A_DATATYPE> &inVec,
+         xrt::hw_context &context, std::vector<C_DATATYPE> &inVec,
          std::vector<C_DATATYPE> &outVec, const int verbosity,
          const int do_verify, const int n_iterations,
          const int n_warmup_iterations, const int trace_size,
          const int b_col_maj) {
   // Fix the seed to ensure reproducibility in CI.
   srand(1726250518); // srand(time(NULL));
+  // Verification tolerance
+  // See "Note on Numerical Tolerances" in README.md
+  float abs_tol = matmul_common::get_abs_tol<C_DATATYPE>();
+  float rel_tol = matmul_common::get_rel_tol<C_DATATYPE>();
 
   int M = vm["M2"].as<int>();
   int K = vm["K2"].as<int>();
@@ -311,7 +319,7 @@ int ffn2(const po::variables_map &vm, xrt::device &device, xrt::xclbin &xclbin,
   int B_VOLUME = N * K;
   int C_VOLUME = M * N;
 
-  size_t A_SIZE = (A_VOLUME * sizeof(A_DATATYPE));
+  size_t A_SIZE = (A_VOLUME * sizeof(C_DATATYPE));
   size_t B_SIZE = (B_VOLUME * sizeof(B_DATATYPE));
   size_t C_SIZE = (C_VOLUME * sizeof(C_DATATYPE));
 
@@ -354,13 +362,13 @@ int ffn2(const po::variables_map &vm, xrt::device &device, xrt::xclbin &xclbin,
     std::cout << "Writing data into buffer objects.\n";
   }
 
-  A_DATATYPE *bufA = bo_a.map<A_DATATYPE *>();
-  std::vector<A_DATATYPE> AVec(A_VOLUME);
+  C_DATATYPE *bufA = bo_a.map<C_DATATYPE *>();
+  std::vector<C_DATATYPE> AVec(A_VOLUME);
   for (int i = 0; i < A_VOLUME; i++) {
-    // AVec[i] = matmul_common::get_random<A_DATATYPE>();
+    // AVec[i] = matmul_common::get_random<C_DATATYPE>();
     AVec[i] = inVec[i];
   }
-  memcpy(bufA, AVec.data(), (AVec.size() * sizeof(A_DATATYPE)));
+  memcpy(bufA, AVec.data(), (AVec.size() * sizeof(C_DATATYPE)));
   B_DATATYPE *bufB = bo_b.map<B_DATATYPE *>();
   std::vector<B_DATATYPE> BVec(B_VOLUME);
   for (int i = 0; i < B_VOLUME; i++) {
@@ -444,12 +452,13 @@ int ffn2(const po::variables_map &vm, xrt::device &device, xrt::xclbin &xclbin,
       if (do_verify_stochastic) {
         errors = matmul_common::verify_stochastic<A_DATATYPE, C_DATATYPE,
                                                   ACC_DATATYPE>(
-            M, N, K, AVec, BVec, CVec, false, verify_stochastic_n_samples,
-            verbosity, abs_tol, rel_tol, b_col_maj);
+            M, N, K, AVec, BVec, CVec, matmul_common::kernel_op_t::MATMUL_OP,
+            verify_stochastic_n_samples, verbosity, abs_tol, rel_tol,
+            b_col_maj);
       } else {
         errors = matmul_common::verify<A_DATATYPE, C_DATATYPE, ACC_DATATYPE>(
-            M, N, K, AVec, BVec, CVec, false, verbosity, abs_tol, rel_tol,
-            b_col_maj);
+            M, N, K, AVec, BVec, CVec, matmul_common::kernel_op_t::MATMUL_OP,
+            verbosity, abs_tol, rel_tol, b_col_maj);
       }
       auto vstop = std::chrono::system_clock::now();
       float vtime =
@@ -521,6 +530,229 @@ int ffn2(const po::variables_map &vm, xrt::device &device, xrt::xclbin &xclbin,
   }
 }
 
+int addandnorm(const po::variables_map &vm, xrt::device &device,
+               xrt::xclbin &xclbin, xrt::hw_context &context,
+               std::vector<A_DATATYPE> &AVec, std::vector<C_DATATYPE> &BVec,
+               std::vector<std::bfloat16_t> &outVec, const int verbosity,
+               const int do_verify, const int n_iterations,
+               const int n_warmup_iterations, const int trace_size,
+               const int b_col_maj) {
+  // Verification tolerance
+  // See "Note on Numerical Tolerances" in README.md
+  float abs_tol = matmul_common::get_abs_tol<std::bfloat16_t>();
+  float rel_tol = matmul_common::get_rel_tol<std::bfloat16_t>();
+  int M = vm["M2"].as<int>();
+  int N = vm["N2"].as<int>();
+  bool do_verify_stochastic = (long long)M * N > verify_stochastic_threshold;
+
+  if (verbosity >= 1) {
+    std::cout << "Matrix size " << M << "x" << N << std::endl;
+  }
+
+  int A_VOLUME = M * N;
+  int B_VOLUME = M * N;
+  int C_VOLUME = M * N;
+
+  size_t A_SIZE = (A_VOLUME * sizeof(A_DATATYPE));
+  size_t B_SIZE = (B_VOLUME * sizeof(C_DATATYPE));
+  size_t C_SIZE = (C_VOLUME * sizeof(std::bfloat16_t));
+
+  size_t OUT_SIZE = C_SIZE + trace_size;
+
+  std::vector<uint32_t> instr_v =
+      matmul_common::load_instr_sequence(vm["instr3"].as<std::string>());
+
+  if (verbosity >= 1)
+    std::cout << "Kernel opcode: " << vm["kernel3"].as<std::string>() << "\n";
+  std::string Node = vm["kernel3"].as<std::string>();
+
+  // Get the kernel from the xclbin
+  auto xkernels = xclbin.get_kernels();
+  auto xkernel = *std::find_if(xkernels.begin(), xkernels.end(),
+                               [Node, verbosity](xrt::xclbin::kernel &k) {
+                                 auto name = k.get_name();
+                                 if (verbosity >= 1) {
+                                   std::cout << "Name: " << name << std::endl;
+                                 }
+                                 return name.rfind(Node, 0) == 0;
+                               });
+  auto kernelName = xkernel.get_name();
+
+  // get a kernel handle
+  if (verbosity >= 1)
+    std::cout << "Getting handle to kernel:" << kernelName << "\n";
+  auto kernel = xrt::kernel(context, kernelName);
+
+  auto bo_instr = xrt::bo(device, instr_v.size() * sizeof(int),
+                          XCL_BO_FLAGS_CACHEABLE, kernel.group_id(1));
+  auto bo_a =
+      xrt::bo(device, A_SIZE, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(3));
+  auto bo_b =
+      xrt::bo(device, B_SIZE, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(4));
+  auto bo_out =
+      xrt::bo(device, OUT_SIZE, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(5));
+
+  if (verbosity >= 1) {
+    std::cout << "Writing data into buffer objects.\n";
+  }
+
+  A_DATATYPE *bufA = bo_a.map<A_DATATYPE *>();
+  memcpy(bufA, AVec.data(), (AVec.size() * sizeof(A_DATATYPE)));
+  C_DATATYPE *bufB = bo_b.map<C_DATATYPE *>();
+  memcpy(bufB, BVec.data(), (BVec.size() * sizeof(C_DATATYPE)));
+
+  // Initialize outputs; bufOut is results matrix plus tracing info
+  char *bufOut = bo_out.map<char *>();
+  std::vector<std::bfloat16_t> CVec(C_VOLUME);
+  memset(bufOut, 0, OUT_SIZE);
+
+  if (verbosity >= 2) {
+    std::cout << "DTYPE_IN  = " XSTR(DTYPE_IN) "\n";
+    std::cout << "DTYPE_OUT = " XSTR(DTYPE_OUT) "\n";
+    std::cout << "Verification tolerance " << abs_tol << " absolute, "
+              << rel_tol << " relative.\n";
+    std::cout << "A = \n";
+    matmul_common::print_matrix(AVec, N);
+    std::cout << "B = \n";
+    matmul_common::print_matrix(BVec, N);
+  }
+
+  // Instruction buffer for DMA configuration
+  void *bufInstr = bo_instr.map<void *>();
+  memcpy(bufInstr, instr_v.data(), instr_v.size() * sizeof(int));
+
+  bo_instr.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_a.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_b.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_out.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+
+  unsigned num_iter = n_iterations + n_warmup_iterations;
+  float npu_time_total = 0;
+  float npu_time_min = 9999999;
+  float npu_time_max = 0;
+  float cpu_time_total = 0;
+
+  int errors = 0;
+  float macs = 2.0 * float(M) * float(N);
+
+  for (unsigned iter = 0; iter < num_iter; iter++) {
+
+    if (verbosity >= 1) {
+      std::cout << "Running Kernel (iteration " << iter << ").\n";
+    }
+    auto start = std::chrono::high_resolution_clock::now();
+    unsigned int opcode = 3;
+    auto run = kernel(opcode, bo_instr, instr_v.size(), bo_a, bo_b, bo_out);
+    ert_cmd_state r = run.wait();
+    if (r != ERT_CMD_STATE_COMPLETED) {
+      std::cout << "Kernel did not complete. Returned status: " << r << "\n";
+      return 1;
+    }
+    auto stop = std::chrono::high_resolution_clock::now();
+    bo_out.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+
+    if (iter < n_warmup_iterations) {
+      /* Warmup iterations do not count towards average runtime. */
+      continue;
+    }
+
+    if (do_verify) {
+      memcpy(CVec.data(), bufOut, (CVec.size() * sizeof(C_DATATYPE)));
+      if (verbosity >= 1) {
+        if (do_verify_stochastic) {
+          std::cout << "Verifying " << verify_stochastic_n_samples
+                    << " random samples against reference matmul ..."
+                    << std::endl;
+        } else {
+          std::cout << "Verifying against reference matmul ..." << std::endl;
+        }
+      }
+      auto vstart = std::chrono::system_clock::now();
+      if (do_verify_stochastic) {
+        errors = matmul_common::verify_stochastic<C_DATATYPE, std::bfloat16_t,
+                                                  float>(
+            M, N, 0, AVec, BVec, CVec,
+            matmul_common::kernel_op_t::ADD_AND_NORM_OP,
+            verify_stochastic_n_samples, verbosity, abs_tol, rel_tol,
+            b_col_maj);
+      } else {
+        errors = matmul_common::verify<C_DATATYPE, std::bfloat16_t, float>(
+            M, N, 0, AVec, BVec, CVec,
+            matmul_common::kernel_op_t::ADD_AND_NORM_OP, verbosity, abs_tol,
+            rel_tol, b_col_maj);
+      }
+      auto vstop = std::chrono::system_clock::now();
+      float vtime =
+          std::chrono::duration_cast<std::chrono::seconds>(vstop - vstart)
+              .count();
+      if (verbosity >= 1) {
+        std::cout << "Verify time: " << vtime << " s." << std::endl;
+      }
+    } else {
+      if (verbosity >= 1)
+        std::cout << "WARNING: matmul results not verified." << std::endl;
+    }
+
+    float npu_time =
+        std::chrono::duration_cast<std::chrono::microseconds>(stop - start)
+            .count();
+
+    // if (iter % 10 == 0) {
+    //   cpu_time_total +=
+    //       matmul_common::time_matmul<A_DATATYPE, C_DATATYPE, ACC_DATATYPE>(
+    //           M, N, K, AVec, BVec, CVec, verbosity, abs_tol, rel_tol,
+    //           b_col_maj);
+    // }
+
+    npu_time_total += npu_time;
+    npu_time_min = (npu_time < npu_time_min) ? npu_time : npu_time_min;
+    npu_time_max = (npu_time > npu_time_max) ? npu_time : npu_time_max;
+  }
+
+  // Only write out trace of last iteration.
+  if (trace_size > 0) {
+    memcpy(CVec.data(), bufOut, (CVec.size() * sizeof(C_DATATYPE)));
+    matmul_common::write_out_trace(((char *)bufOut) + C_SIZE, trace_size,
+                                   vm["trace_file"].as<std::string>());
+  }
+
+  std::cout << std::endl
+            << "Avg NPU matmul time: " << npu_time_total / n_iterations << "us."
+            << std::endl;
+  //   std::cout << "Avg NPU gflops: "
+  //             << macs / (1000 * npu_time_total / n_iterations) << std::endl;
+
+  std::cout << std::endl
+            << "Min NPU matmul time: " << npu_time_min << "us." << std::endl;
+  //   std::cout << "Max NPU gflops: " << macs / (1000 * npu_time_min) <<
+  //   std::endl;
+
+  std::cout << std::endl
+            << "Max NPU matmul time: " << npu_time_max << "us." << std::endl;
+  //   std::cout << "Min NPU gflops: " << macs / (1000 * npu_time_max) <<
+  //   std::endl;
+
+  //   std::cout << std::fixed << std::setprecision(2) << std::endl
+  //             << "Avg CPU matmul time: " << cpu_time_total / (num_iter / 5)
+  //             << "us." << std::endl;
+
+  outVec = CVec;
+  if (!errors) {
+    std::cout << "\nPASS!\n\n";
+    return 0;
+  } else {
+    std::cout << "\nError count: " << errors;
+    if (do_verify_stochastic) {
+      std::cout << " (out of " << verify_stochastic_n_samples
+                << " random samples)";
+    }
+    std::cout << "\n\n";
+
+    std::cout << "\nFailed.\n\n";
+    return 1;
+  }
+}
+
 int main(int argc, const char *argv[]) {
 
   // Program arguments parsing
@@ -559,11 +791,16 @@ int main(int argc, const char *argv[]) {
   if (verbosity >= 1)
     std::cout << "Getting hardware context.\n";
   xrt::hw_context context(device, xclbin.get_uuid());
-  std::vector<A_DATATYPE> in;
-  std::vector<C_DATATYPE> out;
-  int fail = ffn1(vm, device, xclbin, context, in, verbosity, do_verify,
+  std::vector<A_DATATYPE> in1;
+  std::vector<C_DATATYPE> out1;
+  std::vector<C_DATATYPE> out2;
+  std::vector<std::bfloat16_t> out3;
+  int fail = ffn1(vm, device, xclbin, context, in1, out1, verbosity, do_verify,
                   n_iterations, n_warmup_iterations, trace_size, b_col_maj);
-  fail |= ffn2(vm, device, xclbin, context, in, out, verbosity, do_verify,
+  fail |= ffn2(vm, device, xclbin, context, out1, out2, verbosity, do_verify,
                n_iterations, n_warmup_iterations, trace_size, b_col_maj);
+  fail |= addandnorm(vm, device, xclbin, context, in1, out2, out3, verbosity,
+                     do_verify, n_iterations, n_warmup_iterations, trace_size,
+                     b_col_maj);
   return fail;
 }
