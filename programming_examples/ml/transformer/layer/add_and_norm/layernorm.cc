90f496d2b61737597940122540e4c8e2b3613254
kurtis-b <bcurtjohn@gmail.com>
2025-04-19 01:55:51 -0700

    Fixed kernel bugs, still need to fix runtime error when workload scale M/m is not 2

--- programming_examples/ml/transformer/layer/add_and_norm/layernorm.cc
+++ programming_examples/ml/transformer/layer/add_and_norm/layernorm.cc
@@ -7,86 +7,112 @@
 
 #include "add.cc"
 
-float Q_rsqrt(float number) {
-  long i;
-  float x2, y;
-  const float threehalfs = 1.5F;
+// NOTE: The scalar version doesn't work
+// float Q_rsqrt(float number) {
+//   long i;
+//   float x2, y;
+//   const float threehalfs = 1.5F;
 
-  x2 = number * 0.5F;
-  y = number;
-  i = *(long *)&y;
-  i = 0x5f3759df - (i >> 1);
-  y = *(float *)&i;
-  y = y * (threehalfs - (x2 * y * y));
+//   x2 = number * 0.5F;
+//   y = number;
+//   i = *(long *)&y;
+//   i = 0x5f3759df - (i >> 1);
+//   y = *(float *)&i;
+//   y = y * (threehalfs - (x2 * y * y));
 
-  return y;
-}
+//   return y;
+// }
 
-template <typename T_in, typename T_out, const int M, const int N>
-void layer_norm_scalar(T_in *in, T_out *out) {
-  for (int i = 0; i < M; i++) {
-    float sum = aie::to_float(0);
-    float sumsq = aie::to_float(0);
-    for (int j = 0; j < N; j++) {
-      float val = aie::to_float(in[i * N + j]);
-      sum += val;
-      sumsq += val * val;
-    }
-    float mean = sum / aie::to_float(N);
-    float var = (sumsq / aie::to_float(N)) - (mean * mean);
-    float stdev =
-        var * aie::to_float(Q_rsqrt(var)); // Q_rsqrt(var) is 1/sqrt(var)
-    for (int j = 0; j < N; j++) {
-      float val = aie::to_float(in[i * N + j]);
-      float result = (val - mean) / stdev;
-      out[i * N + j] = T_out(aie::to_float(result));
-    }
-  }
-}
+// template <typename T_in, typename T_out, const int M, const int N>
+// void layer_norm_scalar(T_in *in, T_out *out) {
+//   for (int i = 0; i < M; i++) {
+//     float sum = aie::to_float(0);
+//     float sumsq = aie::to_float(0);
+//     for (int j = 0; j < N; j++) {
+//       float val = aie::to_float(in[i * N + j]);
+//       sum += val;
+//       sumsq += val * val;
+//     }
+//     float mean = sum / aie::to_float(N);
+//     float var = (sumsq / aie::to_float(N)) - (mean * mean);
+//     float stdev =
+//         var * aie::to_float(Q_rsqrt(var)); // Q_rsqrt(var) is 1/sqrt(var)
+//     for (int j = 0; j < N; j++) {
+//       float val = aie::to_float(in[i * N + j]);
+//       float result = (val - mean) / stdev;
+//       out[i * N + j] = T_out(aie::to_float(result));
+//     }
+//   }
+// }
 
 // LayerNorm will normalize across the embedding dimension for each token
 template <typename T_in, typename T_out, const int M, const int N>
-void layer_norm_vector(T_in *in, T_out *out) {
-  // The commented portion below is a pass-through for the input
-  //   for (int i = 0; i < M; i++) {
-  //     for (int j = 0; j < N; j++) {
-  //       out[i * N + j] = T_out(aie::to_float(in[i * N + j]));
-  //     }
-  //   }
+void layer_norm_vector(T_in *__restrict in, T_out *__restrict out) {
   constexpr int vec_factor = 16;
   event0();
   const int F = N / vec_factor;
+  // The commented portion below is a pass-through for the input to test that
+  // the input data is correct
+  //   for (int i = 0; i < M; i++) {
+  //     T_in *__restrict pA1_0 = in + i * N;
+  //     T_out *__restrict pC1 = out + i * N;
+  //     for (int j = 0; j < F; j++) {
+  //       aie::vector<T_in, vec_factor> A0 = aie::load_v<vec_factor>(pA1_0);
+  //       pA1_0 += vec_factor;
+  //       aie::accum<acc32, vec_factor> a_acc;
+  //       a_acc.from_vector(A0, 0);
+  //       aie::accum<accfloat, vec_factor> test1 =
+  //           aie::mul(aie::to_float(A0), aie::to_float(1));
+  //       aie::accum<accfloat, vec_factor> test2 =
+  //           aie::mul(aie::to_float(a_acc.to_vector<int32>(0)),
+  //           aie::to_float(1));
+  //       aie::store_v(pC1, test2.to_vector<T_out>(0));
+  //       pC1 += vec_factor;
+  //     }
+  //   }
   for (int i = 0; i < M; i++)
     chess_prepare_for_pipelining chess_loop_range(1, ) {
-      T_in *__restrict pA1 = in + i * N;
+      T_in *__restrict pA1_0 = in + i * N;
+      T_in *__restrict pA1_1 = in + i * N;
       T_out *__restrict pC1 = out + i * N;
-      auto sum = aie::to_float(0);
-      auto sumsq = aie::to_float(0);
+      aie::accum<accfloat, vec_factor> running_total;
+      aie::accum<accfloat, vec_factor> running_sq_total;
+      running_total.from_vector(aie::zeros<float, vec_factor>(), 0);
+      running_sq_total.from_vector(aie::zeros<float, vec_factor>(), 0);
+      float sum = 0;
+      float sumsq = 0;
       for (int j = 0; j < F; j++)
         chess_prepare_for_pipelining chess_loop_range(1, ) {
           aie::vector<float, vec_factor> A0 =
-              aie::to_float(aie::load_v<vec_factor>(pA1));
-          pA1 += vec_factor;
-          aie::vector<float, vec_factor> A0sq =
-              aie::mul(A0, A0).to_vector<float>(0);
-          sum += aie::reduce_add(A0);
-          sumsq += aie::reduce_add(A0sq);
+              aie::to_float(aie::load_v<vec_factor>(pA1_0));
+          pA1_0 += vec_factor;
+          aie::accum<accfloat, vec_factor> a_acc;
+          a_acc.from_vector(A0, 0);
+          running_total = aie::add(running_total, a_acc.to_vector<float>(0));
+          auto sumsq_v =
+              aie::mul(a_acc.to_vector<float>(0), a_acc.to_vector<float>(0));
+          running_sq_total =
+              aie::add(running_sq_total, sumsq_v.to_vector<float>(0));
         }
-      auto mean = aie::div(sum, aie::to_float(N));
-      auto var = aie::div(sumsq, aie::to_float(N)) - (mean * mean);
-      //   auto invstdev = aie::invsqrt(var);
+      sum += aie::reduce_add(running_total.to_vector<float>(0));
+      sumsq += aie::reduce_add(running_sq_total.to_vector<float>(0));
+      float mean = aie::div(sum, aie::to_float(N));
+      float var = aie::div(sumsq, aie::to_float(N)) - (mean * mean);
+      //   aie::vector<T_out, vec_factor> test =
+      //       aie::broadcast<T_out, vec_factor>((T_out)var);
+      //   aie::store_v(pC1, test);
+      //   pC1 += vec_factor;
       v16bfloat16 varVec;
-      varVec[0] = var;
+      varVec[0] = (bfloat16)var;
       aie::vector<bfloat16, 16> invstdev = getRsqrtBf16(varVec);
-      pA1 = in + i * N; // reset pA1 to the start of the row
       for (int j = 0; j < F; j++)
         chess_prepare_for_pipelining chess_loop_range(1, ) {
           aie::vector<float, vec_factor> A0 =
-              aie::to_float(aie::load_v<vec_factor>(pA1));
-          pA1 += vec_factor;
-          aie::accum<accfloat, vec_factor> result_acc;
-          result_acc.from_vector(A0, 0);
-          auto result = aie::sub(result_acc, mean);
+              aie::to_float(aie::load_v<vec_factor>(pA1_1));
+          pA1_1 += vec_factor;
+          aie::accum<accfloat, vec_factor> a_acc;
+          a_acc.from_vector(A0, 0);
+          auto result = aie::sub(a_acc, mean);
           aie::accum<accfloat, vec_factor> result1 =
               aie::mul(result.to_vector<T_out>(0), invstdev[0]);
           aie::store_v(pC1, result1.to_vector<T_out>(0));
