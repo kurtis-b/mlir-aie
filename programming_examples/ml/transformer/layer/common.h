9df18acdadb73dbfb14333b18f51bb16ff8e640a
kurtis-b <bcurtjohn@gmail.com>
2025-04-28 22:44:34 -0700

    Adjusted workload sizes to match GPT-2 hyperparameters, working version of mha still needs work

--- programming_examples/ml/transformer/layer/common.h
+++ programming_examples/ml/transformer/layer/common.h
@@ -627,8 +627,7 @@ int verify(int M, int N, int K, std::vector<Tin> A, std::vector<Tin> B,
   }
   print_error_summary(std::cout, n_errors, errors, max_rel_error);
 
-  //   if (n_errors > 0) {
-  if (1) {
+  if (n_errors > 0) {
     std::cout << std::endl << "Reference:" << std::endl;
     matmul_common::print_matrix(CRef, N);
     std::cout << std::endl << "Output:" << std::endl;
