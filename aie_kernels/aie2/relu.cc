9cacade3987e1c07fa65c2795344db20a1158e19
kurtis-b <bcurtjohn@gmail.com>
2025-05-23 21:52:33 -0700

    Relu kernel now uses the API

--- aie_kernels/aie2/relu.cc
+++ aie_kernels/aie2/relu.cc
@@ -17,23 +17,56 @@
 
 #include <aie_api/aie.hpp>
 
-void relu(bfloat16 *restrict a, bfloat16 *restrict c, const int TILE_SIZE) {
+template <unsigned m, unsigned n>
+void relu_i16(const int16 *__restrict pA, int16 *__restrict pC) {
   const int v_factor = 32;
-  v32bfloat16 zeroes = broadcast_zero_bfloat16();
+  aie::vector<int16, v_factor> zeroes = aie::zeros<int16, v_factor>();
 
   event0();
-  for (size_t i = 0; i < TILE_SIZE; i += v_factor)
-    chess_prepare_for_pipelining chess_loop_range(32, 32) {
-      v32bfloat16 input = *(v32bfloat16 *)(a + i);
-      v32bfloat16 output = max(input, zeroes);
-      *(v32bfloat16 *)(c + i) = output;
+  int16 *__restrict pC1 = pC;
+  for (size_t i = 0; i < m; i++)
+    chess_prepare_for_pipelining {
+      const int16 *__restrict pA1 = pA + i * n;
+      for (size_t j = 0; j < n; j += v_factor) {
+        aie::vector<int16, v_factor> a_vec = aie::load_v<v_factor>(pA1);
+        pA1 += v_factor;
+        aie::vector<int16, v_factor> c_vec = aie::max(a_vec, zeroes);
+        aie::store_v(pC1, c_vec);
+        pC1 += v_factor;
+      }
     }
   event1();
-  return;
+}
+
+template <unsigned m, unsigned n>
+void relu_bf16(bfloat16 *__restrict pA, bfloat16 *__restrict pC) {
+  const int v_factor = 32;
+  aie::vector<bfloat16, v_factor> zeroes = aie::zeros<bfloat16, v_factor>();
+
+  event0();
+  bfloat16 *__restrict pC1 = pC;
+  for (size_t i = 0; i < m; i++)
+    chess_prepare_for_pipelining {
+      const bfloat16 *__restrict pA1 = pA + i * n;
+      for (size_t j = 0; j < n; j += v_factor) {
+        aie::vector<bfloat16, v_factor> a_vec = aie::load_v<v_factor>(pA1);
+        pA1 += v_factor;
+        aie::vector<bfloat16, v_factor> c_vec = aie::max(a_vec, zeroes);
+        aie::store_v(pC1, c_vec);
+        pC1 += v_factor;
+      }
+    }
+  event1();
 }
 
 extern "C" {
 
-void bf16_relu(bfloat16 *a_in, bfloat16 *c_out) { relu(a_in, c_out, 1024); }
+void i16_relu(int16 *a_in, int16 *c_out) {
+  relu_i16<DIM_M, DIM_N>(a_in, c_out);
+}
+
+void bf16_relu(bfloat16 *a_in, bfloat16 *c_out) {
+  relu_bf16<DIM_M, DIM_N>(a_in, c_out);
+}
 
 } // extern "C"
