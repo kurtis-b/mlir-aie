0ae17f84b0ebb96803c31482e356187578591e80
kurtis-b <bcurtjohn@gmail.com>
2025-05-24 00:09:38 -0700

    Add and norm working with updated directory structure

--- aie_kernels/aie2/add.cc
+++ aie_kernels/aie2/add.cc
@@ -17,43 +17,68 @@
 
 #include <aie_api/aie.hpp>
 
-template <typename T_in, typename T_out, const int N>
+template <typename T_in, typename T_out, const int M, const int N>
 void eltwise_add(T_in *a, T_in *b, T_out *c) {
-  for (int i = 0; i < N; i++) {
-    c[i] = a[i] + b[i];
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < N; j++) {
+      c[i * N + j] = (T_out)(a[i * N + j] + b[i * N + j]);
+    }
   }
 }
 
-template <typename T_in, typename T_out, const int N>
+template <typename T_in, typename T_out, const int M, const int N>
 void eltwise_vadd(T_in *a, T_in *b, T_out *c) {
-
   constexpr int vec_factor = 16;
   event0();
-  T_in *__restrict pA1 = a;
-  T_in *__restrict pB1 = b;
-  T_out *__restrict pC1 = c;
   const int F = N / vec_factor;
-  for (int i = 0; i < F; i++)
-    chess_prepare_for_pipelining chess_loop_range(16, ) {
-      aie::vector<T_in, vec_factor> A0 = aie::load_v<vec_factor>(pA1);
-      pA1 += vec_factor;
-      aie::vector<T_in, vec_factor> B0 = aie::load_v<vec_factor>(pB1);
-      pB1 += vec_factor;
-      aie::vector<T_out, vec_factor> cout = aie::add(A0, B0);
-      aie::store_v(pC1, cout);
-      pC1 += vec_factor;
+  for (int i = 0; i < M; i++)
+    chess_prepare_for_pipelining chess_loop_range(1, ) {
+      T_in *__restrict pA1 = a + i * N;
+      T_in *__restrict pB1 = b + i * N;
+      T_out *__restrict pC1 = c + i * N;
+      for (int j = 0; j < F; j++)
+        chess_prepare_for_pipelining chess_loop_range(16, ) {
+          aie::vector<T_in, vec_factor> A0 = aie::load_v<vec_factor>(pA1);
+          pA1 += vec_factor;
+          aie::vector<T_in, vec_factor> B0 = aie::load_v<vec_factor>(pB1);
+          pB1 += vec_factor;
+          aie::vector<T_out, vec_factor> cout =
+              aie::vector_cast<T_out>(aie::add(A0, B0));
+          aie::store_v(pC1, cout);
+          pC1 += vec_factor;
+        }
     }
   event1();
 }
 
 extern "C" {
 
-void eltwise_add_bf16_scalar(bfloat16 *a_in, bfloat16 *b_in, bfloat16 *c_out) {
-  eltwise_add<bfloat16, bfloat16, 1024>(a_in, b_in, c_out);
+#ifndef DIM_M
+#define DIM_M 8
+#endif
+
+#ifndef DIM_N
+#define DIM_N 512
+#endif
+
+// void eltwise_add_i16_scalar(int16 *a_in, int16 *b_in, int16 *c_out) {
+//   eltwise_add<int16, int16, DIM_M, DIM_N>(a_in, b_in, c_out);
+// }
+
+void eltwise_add_i16_vector(int16 *a_in, int16 *b_in, int16 *c_out) {
+  eltwise_vadd<int16, int16, DIM_M, DIM_N>(a_in, b_in, c_out);
 }
 
-void eltwise_add_bf16_vector(bfloat16 *a_in, bfloat16 *b_in, bfloat16 *c_out) {
-  eltwise_vadd<bfloat16, bfloat16, 1024>(a_in, b_in, c_out);
-}
+// void eltwise_add_bf16_scalar(bfloat16 *a_in, bfloat16 *b_in, bfloat16
+// *c_out)
+// {
+//   eltwise_add<bfloat16, bfloat16, DIM_M, DIM_N>(a_in, b_in, c_out);
+// }
+
+// void eltwise_add_bf16_vector(bfloat16 *a_in, bfloat16 *b_in, bfloat16
+// *c_out)
+// {
+//   eltwise_vadd<bfloat16, bfloat16, DIM_M, DIM_N>(a_in, b_in, c_out);
+// }
 
 } // extern "C"
