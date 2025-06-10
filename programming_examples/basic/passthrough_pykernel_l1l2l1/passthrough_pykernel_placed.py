c6e627a5f1920229af54c30bc42df95f06e6c21b
kurtis-b <bcurtjohn@gmail.com>
2025-06-09 22:02:08 -0600

    Example showing the error

--- programming_examples/basic/passthrough_pykernel_l1l2l1/passthrough_pykernel_placed.py
+++ programming_examples/basic/passthrough_pykernel_l1l2l1/passthrough_pykernel_placed.py
@@ -38,7 +38,12 @@ def passthroughKernel(vector_size):
 
         # AIE Core Python Function declarations
         @func(emit=True)
-        def passThroughLine(input: line_ty, output: line_ty, lineWidth: np.int32):
+        def passThroughLine_out1(input: line_ty, output: line_ty, lineWidth: np.int32):
+            for i in range_(lineWidth):
+                output[i] = input[i]
+
+        @func(emit=True)
+        def passThroughLine_out2(input: line_ty, output: line_ty, lineWidth: np.int32):
             for i in range_(lineWidth):
                 output[i] = input[i]
 
@@ -69,10 +74,38 @@ def passthroughKernel(vector_size):
                     ]
                 ])
         object_fifo_link(of_l3l2, of_l2l1)
-        of_l1l2 = object_fifo("out_l1l2", ComputeTile2, MemTile, 2, line_ty)
-        of_l2l3 = object_fifo("out_l2l1", MemTile, ShimTile, 2, memtile_ty)
-        object_fifo_link(of_l1l2, of_l2l3)
-        # of_out = object_fifo("out", ComputeTile3, ShimTile, 2, line_ty)
+        """
+        TODO: I'd like for the MemTile destination in "mid_l1l2" to have a 
+        buffer of size "memtile_ty" like in "of_l3l2", while still having a
+        buffer of size "line_ty" in ComputeTile2. What should be done here? 
+        """
+        mid_l1l2 = object_fifo("mid_l1l2", ComputeTile2, MemTile, 2, line_ty) 
+        """
+        TODO: Repeating what's done above since I'm assuming that the previous
+        compute tile (ComputeTile2) generates a buffer of size "memtile_ty" in
+        "mid_l1l2". But I get an error with the object_fifo below when building.
+        """
+        mid_l2l1 = object_fifo("mid_l2l1", MemTile, ComputeTile3, 2, line_ty, 
+                # 4D transformation in MemTile (MM2S)
+                # Assumes that the "higher" MemTile size
+                # defines the 4D transformation
+                [
+                    (4, lineWidthInBytes),
+                    (lineWidthInBytes // 16, 16),
+                    (4, 4),
+                    (4, 1),
+                ],
+                # 3D transformation in CompTile (S2MM)
+                [
+                    [
+                        (lineWidthInBytes // 16, 16),
+                        (4, 4),
+                        (4, 1),
+                    ]
+                ])
+        object_fifo_link(mid_l1l2, mid_l2l1)
+        out_l1l2 = object_fifo("out_l1l2", ComputeTile3, MemTile, 2, line_ty)
+        out_l2l3 = object_fifo("out_l2l3", MemTile, ShimTile, 2, memtile_ty)
 
 
         # Set up compute tiles
@@ -81,21 +114,21 @@ def passthroughKernel(vector_size):
         @core(ComputeTile2)
         def core_body():
             for _ in range_(sys.maxsize):
-                elemOut = of_l1l2.acquire(ObjectFifoPort.Produce, 1)
+                elemOut = mid_l1l2.acquire(ObjectFifoPort.Produce, 1)
                 elemIn = of_l2l1.acquire(ObjectFifoPort.Consume, 1)
-                passThroughLine(elemIn, elemOut, lineWidthInBytes)
+                passThroughLine_out1(elemIn, elemOut, lineWidthInBytes)
                 of_l2l1.release(ObjectFifoPort.Consume, 1)
-                of_l1l2.release(ObjectFifoPort.Produce, 1)
+                mid_l1l2.release(ObjectFifoPort.Produce, 1)
         
         # Compute tile 3
-        # @core(ComputeTile3)
-        # def core_body():
-        #     for _ in range_(sys.maxsize):
-        #         elemOut = of_out.acquire(ObjectFifoPort.Produce, 1)
-        #         elemIn = of_l2l1.acquire(ObjectFifoPort.Consume, 1)
-        #         passThroughLine(elemIn, elemOut, lineWidthInBytes)
-        #         of_l2l1.release(ObjectFifoPort.Consume, 1)
-        #         of_out.release(ObjectFifoPort.Produce, 1)
+        @core(ComputeTile3)
+        def core_body():
+            for _ in range_(sys.maxsize):
+                elemOut = out_l1l2.acquire(ObjectFifoPort.Produce, 1)
+                elemIn = mid_l2l1.acquire(ObjectFifoPort.Consume, 1)
+                passThroughLine_out2(elemIn, elemOut, lineWidthInBytes)
+                mid_l2l1.release(ObjectFifoPort.Consume, 1)
+                out_l1l2.release(ObjectFifoPort.Produce, 1)
 
         #    print(ctx.module.operation.verify())
 
@@ -104,10 +137,10 @@ def passthroughKernel(vector_size):
         @runtime_sequence(vector_ty, vector_ty, vector_ty)
         def sequence(inTensor, outTensor, notUsed):
             in_task = shim_dma_single_bd_task(
-                of_l3l2, inTensor, sizes=[1, 1, 1, N], issue_token=True
+                of_l3l2, inTensor, sizes=[1, 1, 1, vector_size], issue_token=True
             )
             out_task = shim_dma_single_bd_task(
-                of_l2l3, outTensor, sizes=[1, 1, 1, N], issue_token=True
+                out_l2l3, outTensor, sizes=[1, 1, 1, vector_size], issue_token=True
             )
 
             dma_start_task(in_task, out_task)
