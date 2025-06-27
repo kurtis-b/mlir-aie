9f1c838b452b389853aa54284c7ed76dbacc3624
kurtis-b <bcurtjohn@gmail.com>
2025-06-26 22:46:38 -0600

    Softmax works now

--- aie_kernels/aie2/softmax.cc
+++ aie_kernels/aie2/softmax.cc
@@ -65,11 +65,78 @@ void softmax_simple_bf16(bfloat16 *restrict input_vector,
   return;
 }
 
-extern "C" {
+template <const int M, const int N>
+void softmax_2d_bf16(bfloat16 *restrict input_vector,
+                     bfloat16 *restrict output_vector) {
+  event0();
 
-void softmax_bf16(bfloat16 *restrict input, bfloat16 *restrict output,
-                  const int32_t input_size) {
-  softmax_simple_bf16(input, output, input_size);
+  int row_vector_size = N;
+  for (int i = 0; i < M; i++)
+    chess_prepare_for_pipelining chess_loop_range(1, ) {
+      float accum_exp_val;
+      auto it_exp_in = aie::cbegin_vector<16>((bfloat16 *)input_vector +
+                                              i * row_vector_size);
+      auto it_exp_out = aie::begin_vector<16>((bfloat16 *)output_vector +
+                                              i * row_vector_size);
+      auto it_scale = aie::cbegin_restrict_vector<16>(
+          (bfloat16 *)output_vector + i * row_vector_size);
+      auto it_soft_out = aie::begin_restrict_vector<16>(
+          (bfloat16 *)output_vector + i * row_vector_size);
+
+      bfloat16 col_sum_inv;
+      aie::vector<bfloat16, 16> in_elems, va;
+      aie::accum<accfloat, 16> out_vals;
+      int col_iters = row_vector_size >> 4;
+      accum_exp_val = 0;
+
+      /////////////////////
+      //// Compute exp ////
+      /////////////////////
+      aie::vector<bfloat16, 16> exp_val;
+      aie::vector<float, 16> input_fp32;
+
+      const int elem_iters = row_vector_size / 16;
+      aie::vector<bfloat16, 16> input_bf16;
+      aie::accum<accfloat, 16> exp_val_accum;
+      exp_val_accum = aie::zeros<accfloat, 16>();
+      for (int i = 0; i < elem_iters; i++) {
+        input_bf16 = *it_exp_in++;
+        exp_val = to_v16bfloat16(getExpBf16(input_bf16));
+        exp_val_accum = add(exp_val_accum, exp_val);
+        *it_exp_out++ = exp_val;
+      }
+      aie::vector<float, 16> reduce = exp_val_accum.to_vector<float>();
+      accum_exp_val = aie::reduce_add(reduce);
+      /////////////////////
+
+      col_sum_inv = (bfloat16)aie::inv(accum_exp_val);
+      for (int c = 0; c < col_iters; c++) {
+        in_elems = *it_scale++;
+        out_vals = aie::mul(in_elems, col_sum_inv);
+        *it_soft_out++ = out_vals.to_vector<bfloat16>();
+      }
+    }
+  event1();
+
+  return;
+}
+
+extern "C" {
+#ifndef DIM_M
+#define DIM_M 8
+#endif
+
+#ifndef DIM_N
+#define DIM_N 512
+#endif
+
+// void softmax_bf16(bfloat16 *restrict input, bfloat16 *restrict output,
+//                   const int32_t input_size) {
+//   softmax_simple_bf16(input, output, input_size);
+// }
+
+void softmax_bf16(bfloat16 *restrict input, bfloat16 *restrict output) {
+  softmax_2d_bf16<DIM_M, DIM_N>(input, output);
 }
 
 } // extern "C"
