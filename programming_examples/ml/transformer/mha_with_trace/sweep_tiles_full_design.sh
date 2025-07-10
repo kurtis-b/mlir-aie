c43e7984128a7bb0f648f3eabf599baa09676f7f
kurtis-b <bcurtjohn@gmail.com>
2025-07-09 18:31:43 -0600

    Adding trace data--certain tiles couldn't be traced and certain tiles resulted in empty trace data--need to investigate

---
+++ programming_examples/ml/transformer/mha_with_trace/sweep_tiles_full_design.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# Sweep through a 4-by-4 array of (row, col) tile values
+
+for row in {0..3}; do
+    for col in {0..3}; do
+        export trace_tile="($row,$col)"
+        echo "Running with trace_tile=$trace_tile"
+        make clean
+        make trace
+        mkdir -p traces
+        mv trace_mha.json "traces/trace_mha_${row}_${col}.json"
+    done
+done
