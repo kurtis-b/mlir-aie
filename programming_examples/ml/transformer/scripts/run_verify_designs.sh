63e7bff6ade3f14a9d16dc1784c9f1b1a5ab5808
kurtis-b <bcurtjohn@gmail.com>
2025-07-11 00:41:29 -0600

    Updating results for presentation

--- programming_examples/ml/transformer/scripts/run_verify_designs.sh
+++ programming_examples/ml/transformer/scripts/run_verify_designs.sh
@@ -3,19 +3,20 @@
 output_file="run_verify_results.txt"
 > "$output_file"
 
-for dir in mha add_and_norm ffn-1 ffn-2; do
+for dir in mha mha_by_steps/only_attn_steps mha_by_steps/only_proj_steps add_and_norm ffn-1 ffn-2; do
     if [ -d "$dir" ]; then
         echo "Processing $dir"
+        orig_dir=$(pwd)
         cd "$dir" || continue
         make clean >/dev/null 2>&1
         make run_verif > run_output.txt 2>&1
         status_line=$(grep -E "Failed|PASS!" run_output.txt)
         if [[ -n $status_line ]]; then
-            echo "$dir: $status_line" >> "../$output_file"
+            echo "$dir: $status_line" >> "$orig_dir/$output_file"
         else
-            echo "$dir: No PASS!/Failed status found" >> "../$output_file"
+            echo "$dir: No PASS!/Failed status found" >> "$orig_dir/$output_file"
         fi
-        cd ..
+        cd "$orig_dir" || exit
     fi
 done
 
