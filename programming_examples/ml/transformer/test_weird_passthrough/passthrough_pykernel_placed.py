c6d4acc8e084fda7bb5ad86e3d0710199f69507c
Curt <cbansil@asu.edu>
2025-08-29 05:19:38 -0600

    Test with different obj fifo depth from L1->L2->L1

---
+++ programming_examples/ml/transformer/test_weird_passthrough/passthrough_pykernel_placed.py
@@ -0,0 +1,117 @@
+# passthrough_pykernel/passthrough_pykernel_placed.py -*- Python -*-
+#
+# This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# (c) Copyright 2024 Advanced Micro Devices, Inc. or its affiliates
+import numpy as np
+import sys
+
+from aie.dialects.aie import *
+from aie.dialects.aiex import *
+from aie.extras.context import mlir_mod_ctx
+from aie.helpers.dialects.ext.func import func
+from aie.helpers.dialects.ext.scf import _for as range_
+
+
+dev = AIEDevice.npu1_1col
+
+if len(sys.argv) > 2:
+    if sys.argv[2] == "npu":
+        dev = AIEDevice.npu1_1col
+    elif sys.argv[2] == "npu2":
+        dev = AIEDevice.npu2
+    else:
+        raise ValueError("[ERROR] Device name {} is unknown".format(sys.argv[2]))
+
+
+def passthroughKernel(vector_size):
+    N = vector_size
+    lineWidthInBytes = N // 4  # chop input in 4 sub-tensors
+
+    @device(dev)
+    def device_body():
+        # define types
+        line_ty = np.ndarray[(lineWidthInBytes,), np.dtype[np.uint16]]
+
+        # AIE Core Python Function declarations
+        @func(emit=True)
+        def passThroughLine(input: line_ty, output: line_ty, lineWidth: np.int32):
+            for i in range_(lineWidth):
+                output[i] = input[i]
+
+        @func(emit=True)
+        def passThroughLine1(input: np.ndarray[(lineWidthInBytes // 2,), np.dtype[np.uint16]], output: np.ndarray[(lineWidthInBytes // 2,), np.dtype[np.uint16]], lineWidth: np.int32):
+            for i in range_(lineWidth):
+                output[i] = input[i]
+
+        # Tile declarations
+        ShimTile = tile(0, 0)
+        MemTile = tile(0, 1)
+        ComputeTile2 = tile(0, 2)
+        ComputeTile3 = tile(0, 3)
+
+        # AIE-array data movement with object fifos
+        of_in1 = object_fifo("in1", ShimTile, MemTile, [2, 2], np.ndarray[(lineWidthInBytes,), np.dtype[np.uint16]])
+        of_in2 = object_fifo("in2", MemTile, ComputeTile2, [2, 2], np.ndarray[(lineWidthInBytes,), np.dtype[np.uint16]])
+        of_out1 = object_fifo("out1", ComputeTile2, MemTile, [2, 2], np.ndarray[(lineWidthInBytes,), np.dtype[np.uint16]])
+        of_out2 = object_fifo("out2", MemTile, ComputeTile3, [4, 4], np.ndarray[(lineWidthInBytes // 2,), np.dtype[np.uint16]])
+        of_out3 = object_fifo("out3", ComputeTile3, MemTile, [4, 4], np.ndarray[(lineWidthInBytes // 2,), np.dtype[np.uint16]])
+        of_out4 = object_fifo("out4", MemTile, ShimTile, [2, 2], np.ndarray[(lineWidthInBytes,), np.dtype[np.uint16]])
+        object_fifo_link(of_in1, of_in2)
+        object_fifo_link(of_out1, of_out2)
+        object_fifo_link(of_out3, of_out4)
+
+        # Set up compute tiles
+        # By changing the array dimensions in out2, it seems like we essentially have an implicit offset when out1 produces data
+        # and out2 consumes it. This also seems to be the case when out3 produces data and out4 consumes it.
+
+        # Compute tile 2
+        @core(ComputeTile2)
+        def core_body():
+            for _ in range_(sys.maxsize):
+                elemOut = of_out1.acquire(ObjectFifoPort.Produce, 2)
+                elemIn = of_in2.acquire(ObjectFifoPort.Consume, 2)
+                passThroughLine(elemIn[0], elemOut[0], lineWidthInBytes)
+                passThroughLine(elemIn[1], elemOut[1], lineWidthInBytes)
+                of_in2.release(ObjectFifoPort.Consume, 2)
+                of_out1.release(ObjectFifoPort.Produce, 2)
+
+        # Compute tile 3
+        @core(ComputeTile3)
+        def core_body():
+            for _ in range_(sys.maxsize):
+                elemIn = of_out2.acquire(ObjectFifoPort.Consume, 2)
+                elemOut = of_out3.acquire(ObjectFifoPort.Produce, 2)
+                passThroughLine1(elemIn[0], elemOut[0], lineWidthInBytes // 2)
+                passThroughLine1(elemIn[1], elemOut[1], lineWidthInBytes // 2)
+                of_out3.release(ObjectFifoPort.Produce, 2)
+                of_out2.release(ObjectFifoPort.Consume, 2)
+        #    print(ctx.module.operation.verify())
+
+        vector_ty = np.ndarray[(N,), np.dtype[np.uint16]]
+
+        @runtime_sequence(vector_ty, vector_ty, vector_ty)
+        def sequence(inTensor, outTensor, notUsed):
+            in_task = shim_dma_single_bd_task(
+                of_in1, inTensor, sizes=[1, 1, 1, N], issue_token=True
+            )
+            out_task = shim_dma_single_bd_task(
+                of_out4, outTensor, sizes=[1, 1, 1, N], issue_token=True
+            )
+
+            dma_start_task(in_task, out_task)
+            dma_await_task(in_task, out_task)
+
+
+try:
+    vector_size = int(sys.argv[1])
+    if vector_size % 64 != 0 or vector_size < 512:
+        print("Vector size must be a multiple of 64 and greater than or equal to 512")
+        raise ValueError
+except ValueError:
+    print("Argument has inappropriate value")
+with mlir_mod_ctx() as ctx:
+    passthroughKernel(vector_size)
+    print(ctx.module)
---
+++ programming_examples/ml/transformer/test_weird_passthrough/test.cpp
@@ -0,0 +1,129 @@
+//===- test.cpp -------------------------------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// Copyright (C) 2023, Advanced Micro Devices, Inc.
+//
+//===----------------------------------------------------------------------===//
+
+#include "cxxopts.hpp"
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include "test_utils.h"
+#include "xrt/xrt_bo.h"
+
+#ifndef DATATYPES_USING_DEFINED
+#define DATATYPES_USING_DEFINED
+// ------------------------------------------------------
+// Configure this to match your buffer data type
+// ------------------------------------------------------
+using DATATYPE = std::uint16_t;
+#endif
+
+int main(int argc, const char *argv[]) {
+  // Program arguments parsing
+  cxxopts::Options options("Passthrough PyKernel Test",
+                           "Test the Passthrough PyKernel");
+  cxxopts::ParseResult vm;
+  test_utils::add_default_options(options);
+
+  test_utils::parse_options(argc, argv, options, vm);
+  int verbosity = vm["verbosity"].as<int>();
+  int trace_size = vm["trace_sz"].as<int>();
+
+  std::cout << std::endl << "Running...";
+
+  // Load instruction sequence
+  std::vector<uint32_t> instr_v =
+      test_utils::load_instr_binary(vm["instr"].as<std::string>());
+
+  if (verbosity >= 1)
+    std::cout << "Sequence instr count: " << instr_v.size() << "\n";
+
+  // Start the XRT context and load the kernel
+  xrt::device device;
+  xrt::kernel kernel;
+
+  test_utils::init_xrt_load_kernel(device, kernel, verbosity,
+                                   vm["xclbin"].as<std::string>(),
+                                   vm["kernel"].as<std::string>());
+
+  // set up the buffer objects
+  auto bo_instr = xrt::bo(device, instr_v.size() * sizeof(int),
+                          XCL_BO_FLAGS_CACHEABLE, kernel.group_id(1));
+  auto bo_inA = xrt::bo(device, PASSTHROUGH_SIZE * sizeof(DATATYPE),
+                        XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(3));
+  auto bo_out =
+      xrt::bo(device, PASSTHROUGH_SIZE * sizeof(DATATYPE) + trace_size,
+              XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(3));
+
+  if (verbosity >= 1)
+    std::cout << "Writing data into buffer objects.\n";
+
+  // Copy instruction stream to xrt buffer object
+  void *bufInstr = bo_instr.map<void *>();
+  memcpy(bufInstr, instr_v.data(), instr_v.size() * sizeof(int));
+
+  // Initialize buffer bo_inA
+  DATATYPE *bufInA = bo_inA.map<DATATYPE *>();
+  for (int i = 0; i < PASSTHROUGH_SIZE; i++)
+  {
+    bufInA[i] = i;
+    if (i < 10 || i > PASSTHROUGH_SIZE - 10)
+      std::cout << "Initializing bo_inA[" << i << "] = " << std::to_string(bufInA[i]) << std::endl;
+  }
+
+  // Zero out buffer bo_out
+  DATATYPE *bufOut = bo_out.map<DATATYPE *>();
+  memset(bufOut, 0, PASSTHROUGH_SIZE * sizeof(DATATYPE) + trace_size);
+
+  // sync host to device memories
+  bo_instr.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_inA.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_out.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+
+  // Execute the kernel and wait to finish
+  if (verbosity >= 1)
+    std::cout << "Running Kernel.\n";
+  unsigned int opcode = 3;
+  auto run = kernel(opcode, bo_instr, instr_v.size(), bo_inA, bo_out);
+  run.wait();
+
+  // Sync device to host memories
+  bo_out.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+
+  // Compare out to in
+  int errors = 0;
+  for (int i = 0; i < PASSTHROUGH_SIZE; i++) {
+    if (i < 10 || i > PASSTHROUGH_SIZE - 10)
+      std::cout << "Expected " << std::to_string(bufInA[i])
+                << ", got " << std::to_string(bufOut[i]) << std::endl;
+    if (bufOut[i] != bufInA[i])
+    {
+      errors++;
+    }
+  }
+
+  if (trace_size > 0) {
+    test_utils::write_out_trace(((char *)bufOut) +
+                                    (PASSTHROUGH_SIZE * sizeof(DATATYPE)),
+                                trace_size, vm["trace_file"].as<std::string>());
+  }
+
+  // Print Pass/Fail result of our test
+  if (!errors) {
+    std::cout << std::endl << "PASS!" << std::endl << std::endl;
+    return 0;
+  } else {
+    std::cout << std::endl
+              << errors << " mismatches." << std::endl
+              << std::endl;
+    std::cout << std::endl << "fail." << std::endl << std::endl;
+    return 1;
+  }
+}
