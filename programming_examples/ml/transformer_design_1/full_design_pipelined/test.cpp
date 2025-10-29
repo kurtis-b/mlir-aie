51bd7c1a98cef82f1e3ca68b21929cf4b5c70aa8
Curt <cbansil@asu.edu>
2025-10-29 06:53:31 -0600

    Adding MHA pipeline for 64 tokens

---
+++ programming_examples/ml/transformer_design_1/full_design_pipelined/test.cpp
@@ -0,0 +1,343 @@
+//===- test.cpp -------------------------------------------000---*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// Copyright (C) 2023, Advanced Micro Devices, Inc.
+//
+//===----------------------------------------------------------------------===//
+
+#include "cxxopts.hpp"
+#include <bits/stdc++.h>
+#include <chrono>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <stdfloat>
+
+#include "xrt/xrt_bo.h"
+#include "xrt/xrt_device.h"
+#include "xrt/xrt_kernel.h"
+
+#include "utils.h"
+
+#ifndef DATATYPES_USING_DEFINED
+#define DATATYPES_USING_DEFINED
+#ifndef DTYPE_IN
+#define DTYPE_IN std::bfloat16_t
+#endif
+#ifndef DTYPE_OUT
+#define DTYPE_OUT std::bfloat16_t
+#endif
+#ifndef DTYPE_ACC
+#define DTYPE_ACC float
+#endif
+using A_DATATYPE = DTYPE_IN;
+using B_DATATYPE = DTYPE_IN;
+using C_DATATYPE = DTYPE_OUT;
+using ACC_DATATYPE = DTYPE_ACC;
+#endif
+
+#define XSTR(X) STR(X)
+#define STR(X) #X
+
+constexpr long long verify_stochastic_threshold = 1024 * 1024 * 1024;
+constexpr int verify_stochastic_n_samples = 1000;
+
+// Verification tolerance
+// See "Note on Numerical Tolerances" in README.md
+float abs_tol = mha_common::get_abs_tol<C_DATATYPE>();
+float rel_tol = mha_common::get_rel_tol<C_DATATYPE>();
+
+int main(int argc, const char *argv[]) {
+  // Program arguments parsing
+  cxxopts::Options options("Matrix Matrix Multiplication Test");
+  cxxopts::ParseResult vm;
+  mha_common::add_default_options(options);
+
+  mha_common::parse_options(argc, argv, options, vm);
+  int verbosity = vm["verbosity"].as<int>();
+  int do_verify = vm["verify"].as<int>();
+  int n_iterations = vm["iters"].as<int>();
+  int n_warmup_iterations = vm["warmup"].as<int>();
+  int trace_size = vm["trace_sz"].as<int>();
+  int b_col_maj = vm["b_col_maj"].as<int>();
+
+  // Fix the seed to ensure reproducibility in CI.
+  srand(1726250518); // srand(time(NULL));
+
+  int M = vm["M"].as<int>();
+  int K = vm["K"].as<int>();
+  int N = vm["N"].as<int>();
+  int H = vm["H"].as<int>();
+
+  if (verbosity >= 1) {
+    std::cout << "Matrix size " << M << "x" << K << "x" << N << ", H: " << H << std::endl;
+  }
+
+  int A_VOLUME = M * K + 2 * K * N;
+  int B_VOLUME = 2 * K * N;
+  int C_VOLUME = 4 * M * N;
+
+  size_t A_SIZE = (A_VOLUME * sizeof(A_DATATYPE));
+  size_t B_SIZE = (B_VOLUME * sizeof(B_DATATYPE));
+  size_t C_SIZE = (C_VOLUME * sizeof(C_DATATYPE));
+
+  std::vector<uint32_t> instr_v =
+      test_utils::load_instr_binary(vm["instr"].as<std::string>());
+
+  if (verbosity >= 1)
+    std::cout << "Sequence instr count: " << instr_v.size() << "\n";
+
+  // Start the XRT test code
+  // Get a device handle
+  unsigned int device_index = 0;
+  auto device = xrt::device(device_index);
+
+  // Load the xclbin
+  if (verbosity >= 1)
+    std::cout << "Loading xclbin: " << vm["xclbin"].as<std::string>() << "\n";
+  auto xclbin = xrt::xclbin(vm["xclbin"].as<std::string>());
+
+  if (verbosity >= 1)
+    std::cout << "Kernel opcode: " << vm["kernel"].as<std::string>() << "\n";
+  std::string Node = vm["kernel"].as<std::string>();
+
+  // Get the kernel from the xclbin
+  auto xkernels = xclbin.get_kernels();
+  auto xkernel = *std::find_if(xkernels.begin(), xkernels.end(),
+                               [Node, verbosity](xrt::xclbin::kernel &k) {
+                                 auto name = k.get_name();
+                                 if (verbosity >= 1) {
+                                   std::cout << "Name: " << name << std::endl;
+                                 }
+                                 return name.rfind(Node, 0) == 0;
+                               });
+  auto kernelName = xkernel.get_name();
+
+  if (verbosity >= 1)
+    std::cout << "Registering xclbin: " << vm["xclbin"].as<std::string>()
+              << "\n";
+
+  device.register_xclbin(xclbin);
+
+  // get a hardware context
+  if (verbosity >= 1)
+    std::cout << "Getting hardware context.\n";
+  xrt::hw_context context(device, xclbin.get_uuid());
+
+  // get a kernel handle
+  if (verbosity >= 1)
+    std::cout << "Getting handle to kernel:" << kernelName << "\n";
+  auto kernel = xrt::kernel(context, kernelName);
+
+  auto bo_instr = xrt::bo(device, instr_v.size() * sizeof(int),
+                          XCL_BO_FLAGS_CACHEABLE, kernel.group_id(1));
+  auto bo_a =
+      xrt::bo(device, A_SIZE, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(3));
+  auto bo_b =
+      xrt::bo(device, B_SIZE, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(4));
+  auto bo_out =
+      xrt::bo(device, C_SIZE, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(5));
+
+  auto bo_tmp1 = xrt::bo(device, 1, XRT_BO_FLAGS_HOST_ONLY, kernel.group_id(6));
+
+  // Workaround so we declare a really small trace buffer when one is not used
+  int tmp_trace_size = (trace_size > 0) ? trace_size : 1;
+  auto bo_trace = xrt::bo(device, tmp_trace_size * 4, XRT_BO_FLAGS_HOST_ONLY,
+                          kernel.group_id(7));
+
+  if (verbosity >= 1) {
+    std::cout << "Writing data into buffer objects.\n";
+  }
+
+  A_DATATYPE *bufA = bo_a.map<A_DATATYPE *>();
+  std::vector<A_DATATYPE> AVec(A_VOLUME);
+  for (int i = 0; i < M * K; i++) {
+    AVec[i] = mha_common::get_random<A_DATATYPE>();
+    // if (i % K == i / K) {
+    //   AVec[i] = 1;
+    // } else {
+    //   AVec[i] = 1; // Zero out the rest of the matrix
+    // }
+  }
+  for (int i = M * K; i < ((M * K) + (K * N)); i++) {
+    AVec[i] = mha_common::get_random<B_DATATYPE>();
+    // Write to the anti-diagonal (top right to bottom left)
+    // int row = (i - (M * K + K * N)) / N;
+    // int col = (i - (M * K + K * N)) % N;
+    // if (row + col == N - 1) {
+    // if (i % ((M * K) + N) == i / ((M * K) + N)) {
+    //   AVec[i] = 1;
+    // } else {
+    //   AVec[i] = 1; // Zero out the rest of the matrix
+    // }
+  }
+  for (int i = ((M * K) + (K * N)); i < A_VOLUME; i++) {
+    AVec[i] = mha_common::get_random<B_DATATYPE>();
+    // Write to the anti-diagonal (top right to bottom left)
+    // int row = (i - (M * K + K * N)) / N;
+    // int col = (i - (M * K + K * N)) % N;
+    // if (row + col == N - 1) {
+    // if (i % ((M * K) + (K * N) + N) == i / ((M * K) + (K * N) + N)) {
+    //   AVec[i] = 1;
+    // } else {
+    //   AVec[i] = 1; // Zero out the rest of the matrix
+    // }
+  }
+  memcpy(bufA, AVec.data(), (AVec.size() * sizeof(A_DATATYPE)));
+  B_DATATYPE *bufB = bo_b.map<B_DATATYPE *>();
+  std::vector<B_DATATYPE> BVec(B_VOLUME);
+  for (int i = 0; i < K * N; i++) {
+    BVec[i] = mha_common::get_random<B_DATATYPE>();
+  }
+  for (int i = K * N; i < B_VOLUME; i++) {
+    BVec[i] = mha_common::get_random<B_DATATYPE>();
+  }
+  memcpy(bufB, BVec.data(), (BVec.size() * sizeof(B_DATATYPE)));
+
+  // Initialize outputs; bufOut is results matrix plus tracing info
+  char *bufOut = bo_out.map<char *>();
+  std::vector<C_DATATYPE> CVec(C_VOLUME);
+  memset(bufOut, 0, C_SIZE);
+
+  //   for (int i = 0; i < M * N; ++i) {
+  //     int col = i % N;
+  //     CVec[i] =
+  //         (col == 0) ? static_cast<C_DATATYPE>(1) :
+  //         static_cast<C_DATATYPE>(0);
+  //   }
+  //   for (int i = M * N; i < 2 * M * N; ++i) {
+  //     int row = (i - M * N) / N;
+  //     CVec[i] = static_cast<C_DATATYPE>(row);
+  //   }
+  //   memcpy(bufOut, CVec.data(), (CVec.size() * sizeof(C_DATATYPE)));
+
+  char *bufTrace = bo_trace.map<char *>();
+  if (trace_size > 0)
+    memset(bufTrace, 0, trace_size);
+
+  if (verbosity >= 2) {
+    std::cout << "DTYPE_IN  = " XSTR(DTYPE_IN) "\n";
+    std::cout << "DTYPE_OUT = " XSTR(DTYPE_OUT) "\n";
+    std::cout << "Verification tolerance " << abs_tol << " absolute, "
+              << rel_tol << " relative.\n";
+    std::cout << "A = \n";
+    // mha_common::print_matrix(AVec, K);
+    std::cout << "B = \n";
+    // mha_common::print_matrix(BVec, N);
+  }
+
+  // Instruction buffer for DMA configuration
+  void *bufInstr = bo_instr.map<void *>();
+  memcpy(bufInstr, instr_v.data(), instr_v.size() * sizeof(int));
+
+  bo_instr.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_a.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_b.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  bo_out.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  if (trace_size > 0)
+    bo_trace.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+
+  unsigned num_iter = n_iterations + n_warmup_iterations;
+  float npu_time_total = 0;
+  float npu_time_min = 9999999;
+  float npu_time_max = 0;
+
+  int errors = 0;
+  float macs = 2.0 * (3.0 * float(M) * float(K) * float(N) +
+                      2.0 * float(M) * float(N) * float(M));
+
+  for (unsigned iter = 0; iter < num_iter; iter++) {
+
+    if (verbosity >= 1) {
+      std::cout << "Running Kernel (iteration " << iter << ").\n";
+    }
+    auto start = std::chrono::high_resolution_clock::now();
+    unsigned int opcode = 3;
+    auto run = kernel(opcode, bo_instr, instr_v.size(), bo_a, bo_b, bo_out,
+                      bo_tmp1, bo_trace);
+    ert_cmd_state r = run.wait();
+    if (r != ERT_CMD_STATE_COMPLETED) {
+      std::cout << "Kernel did not complete. Returned status: " << r << "\n";
+      return 1;
+    }
+    auto stop = std::chrono::high_resolution_clock::now();
+    bo_out.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+    if (trace_size > 0)
+      bo_trace.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+
+    if (iter < n_warmup_iterations) {
+      /* Warmup iterations do not count towards average runtime. */
+      continue;
+    }
+
+    if (do_verify) {
+      memcpy(CVec.data(), bufOut, (CVec.size() * sizeof(C_DATATYPE)));
+      if (verbosity >= 1) {
+        std::cout << "Verifying against reference matmul ..." << std::endl;
+      }
+      auto vstart = std::chrono::system_clock::now();
+      errors = mha_common::verify<A_DATATYPE, C_DATATYPE, ACC_DATATYPE>(
+          M, N, K, H, AVec, BVec, CVec, verbosity, abs_tol, rel_tol, b_col_maj);
+      auto vstop = std::chrono::system_clock::now();
+      float vtime =
+          std::chrono::duration_cast<std::chrono::seconds>(vstop - vstart)
+              .count();
+      if (verbosity >= 1) {
+        std::cout << "Verify time: " << vtime << " s." << std::endl;
+      }
+    } else {
+      if (verbosity >= 1)
+        std::cout << "WARNING: matmul results not verified." << std::endl;
+    }
+
+    float npu_time =
+        std::chrono::duration_cast<std::chrono::microseconds>(stop - start)
+            .count();
+
+    if (n_iterations <= 5) {
+      std::cout << "NPU matmul time: " << npu_time << "us." << std::endl;
+      std::cout << "NPU gflops: " << macs / (1000 * npu_time) << std::endl;
+    }
+    npu_time_total += npu_time;
+    npu_time_min = (npu_time < npu_time_min) ? npu_time : npu_time_min;
+    npu_time_max = (npu_time > npu_time_max) ? npu_time : npu_time_max;
+  }
+
+  // Only write out trace of last iteration.
+  if (trace_size > 0) {
+    mha_common::write_out_trace((char *)bufTrace, trace_size,
+                                   vm["trace_file"].as<std::string>());
+  }
+
+  std::cout << std::endl
+            << "Avg NPU matmul time: " << npu_time_total / n_iterations << "us."
+            << std::endl;
+  std::cout << "Avg NPU gflops: "
+            << macs / (1000 * npu_time_total / n_iterations) << std::endl;
+
+  std::cout << std::endl
+            << "Min NPU matmul time: " << npu_time_min << "us." << std::endl;
+  std::cout << "Max NPU gflops: " << macs / (1000 * npu_time_min) << std::endl;
+
+  std::cout << std::endl
+            << "Max NPU matmul time: " << npu_time_max << "us." << std::endl;
+  std::cout << "Min NPU gflops: " << macs / (1000 * npu_time_max) << std::endl;
+
+  if (!errors) {
+    std::cout << "\nPASS!\n\n";
+    return 0;
+  } else {
+    std::cout << "\nError count: " << errors;
+    std::cout << "\n\n";
+
+    std::cout << "\nFailed.\n\n";
+    return 1;
+  }
+}
